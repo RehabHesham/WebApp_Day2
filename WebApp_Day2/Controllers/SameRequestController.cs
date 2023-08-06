3b5c27053e7e261c4e9a87220f04ab76d2f1718d
Rehab Hesham <rehabhesham98@yahoo.com>
2023-08-06 10:51:00 +0300

    Day4_part1

---
+++ WebApp_Day2/Controllers/SameRequestController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp_Day2.Models;
+using WebApp_Day2.ViewModels;
+
+namespace WebApp_Day2.Controllers
+{
+    public class SameRequestController : Controller
+    {
+        ITIContext context;
+        public SameRequestController()
+        {
+            context = new ITIContext();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ViewDataAction(string name, int age)
+        {
+            ViewData["name"] = name;
+            ViewData["age"] = age;
+            ViewData["arr"] = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            return View("ViewData");
+        }
+
+
+        public IActionResult ViewBagAction(string name, int age)
+        {
+            ViewBag.name = name;
+            ViewBag.age = age;
+            ViewBag.arr = new int[] { 1, 2, 3, 4, 5, 6 };
+
+            ViewData["address"] = "Alex";
+            ViewBag.location = "Giza";
+            return View();
+        }
+
+        public IActionResult ModelAction(string name, int age)
+        {
+            // create object
+            // anonymous object
+            //var obj = new { Name = name, Age = age };
+            //return View(obj);
+
+            // object using class
+            Person person = new Person() { Name=name,Age=age};
+            return View(person);
+        }
+
+        public IActionResult ViewModelAction()
+        {
+            Course course = context.Courses.Include(c=>c.CourseStudents).ThenInclude(cs=>cs.Student).FirstOrDefault();
+
+
+            CourseStudents students = new CourseStudents()
+            {
+                Crs_Id = course.ID,
+                Crs_Name = course.Name
+            };
+
+            for(int i=0; i<course.CourseStudents.Count; i++)
+            {
+                students.stds.Add(new Students()
+                {
+                    Std_Id = course.CourseStudents[i].Std_ID,
+                    Std_Name = course.CourseStudents[i].Student.Name,
+                    Grade = course.CourseStudents[i].Grade
+                });
+            }
+            return View(students);
+        }
+    }
+}
---
+++ WebApp_Day2/ViewModels/CourseStudents.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApp_Day2.ViewModels
+{
+    public class CourseStudents
+    {
+        public int Crs_Id { get; set; }
+
+        public string Crs_Name { get; set; }
+
+       public List<Students> stds { get; set; }    = new List<Students>();
+    }
+
+    public class Students
+    {
+        public int Std_Id { get; set; }
+        public string Std_Name { get; set; }
+
+        public int Grade { get; set; }
+    }
+}
